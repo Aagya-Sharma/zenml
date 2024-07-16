c13b2c1aa64a4d79280fa1f451b7b1c37eac8a28
Aagya-Sharma <sharmaaagya7@gmail.com>
2024-07-16 15:54:58 +0545

    define the api paths for all secrets

---
+++ src/types/secret.ts
@@ -0,0 +1,5 @@
+import { components } from "./core";
+
+export type SecretNamespace = components["schemas"]["SecretResponse"];
+export type SecretNamespaceBody = components["schemas"]["SecretResponseBody"];
+export type SecretResources = components["schemas"]["SecretResponseResources"];
