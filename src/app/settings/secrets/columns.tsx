eec9883cfb297752084538c9531b3804ae69fa65
Aagya-Sharma <sharmaaagya7@gmail.com>
2024-07-17 13:02:07 +0545

    initial commit

--- src/app/settings/secrets/columns.tsx
+++ src/app/settings/secrets/columns.tsx
@@ -1,15 +1,53 @@
 import { DisplayDate } from "@/components/DisplayDate";
+import { InlineAvatar } from "@/components/InlineAvatar";
 import { SecretNamespace } from "@/types/secret";
 import { ColumnDef } from "@tanstack/react-table";
+import SecretsDropdown from "./SecretsDropdown";
+import LockIcon from "@/assets/icons/lock-icon.svg?react";
+import InfoIcon from "@/assets/icons/info-icon.svg?react";
 
 export function getSecretColumns(): ColumnDef<SecretNamespace>[] {
 	return [
+		{
+			id: "select",
+			header: ({ table }) => (
+				<input
+					type="checkbox"
+					style={{ borderRadius: 5 }}
+					{...{
+						checked: table.getIsAllPageRowsSelected(),
+						onChange: table.getToggleAllPageRowsSelectedHandler()
+					}}
+				/>
+			),
+			cell: ({ row }) => (
+				<input
+					type="checkbox"
+					style={{ borderRadius: 5 }}
+					{...{
+						checked: row.getIsSelected(),
+						onChange: row.getToggleSelectedHandler()
+					}}
+				/>
+			)
+		},
 		{
 			id: "secret",
 			header: "Secret",
 			accessorFn: (row) => row.name,
 			cell: ({ getValue }) => (
-				<p className="text-text-sm text-theme-text-secondary">{getValue<string>()}</p>
+				<div className="flex items-center space-x-2">
+					<LockIcon />
+					<div className="flex flex-col">
+						<div className="flex flex-row space-x-1">
+							<span className="text-text-sm font-semibold text-theme-text-primary">
+								{getValue<string>()}
+							</span>
+							<InfoIcon />
+						</div>
+						<span className="text-sm text-gray-500">85ff662e</span>
+					</div>
+				</div>
 			)
 		},
 		{
@@ -18,7 +56,13 @@ export function getSecretColumns(): ColumnDef<SecretNamespace>[] {
 		},
 		{
 			id: "author",
-			header: "Author"
+			header: "Author",
+			accessorFn: (row) => row.body?.user?.body?.full_name,
+			cell: ({ getValue }) => (
+				<>
+					<InlineAvatar username={getValue<string>()} />
+				</>
+			)
 		},
 		{
 			id: "created_at",
@@ -29,6 +73,17 @@ export function getSecretColumns(): ColumnDef<SecretNamespace>[] {
 					<DisplayDate dateString={getValue<string>()} />
 				</p>
 			)
+		},
+
+		{
+			id: "admin_actions",
+			header: "Actions",
+			cell: ({ row }) =>
+				row.original.body?.user?.body?.is_admin ? (
+					<SecretsDropdown></SecretsDropdown>
+				) : (
+					<p className="text-text-sm text-theme-text-secondary">No Actions</p>
+				)
 		}
 	];
 }
--- src/types/secret.ts
+++ src/types/secret.ts
@@ -1,5 +1,3 @@
 import { components } from "./core";
 
 export type SecretNamespace = components["schemas"]["SecretResponse"];
-export type SecretNamespaceBody = components["schemas"]["SecretResponseBody"];
-export type SecretResources = components["schemas"]["SecretResponseResources"];
